8fa377eb7f6b29d08e675338dd246396a5ada1a8
Enas Siddiqi <enas.siddiqi@ghi.community>
2018-10-15 18:02:31 -0500

    Nomination form and mailer

--- assets/js/forms/org.js
+++ assets/js/forms/org.js
@@ -17,11 +17,11 @@ var OrgForm = function () {
                     {
                         required: true
                     },
+                    // member:
+                    // {
+                    //   required: true  
+                    // },
                     orgName:
-                    {
-                        required: true
-                    },
-                    title:
                     {
                         required: true
                     },
@@ -29,12 +29,11 @@ var OrgForm = function () {
                     {
                         required: true
                     },
-                    email:
+                    city:
                     {
-                        required: true,
-                        email: true
+                        required: false
                     },
-                    phone:
+                    state:
                     {
                         required: false
                     },
@@ -62,11 +61,11 @@ var OrgForm = function () {
                     },
                     orgName:
                     {
-                        required: 'Please enter your organization name'
+                        required: 'Please enter the organization name'
                     },
-                    title:
+                    ein:
                     {
-                        required: 'Please enter your title or role'
+                        required: 'Please enter the organization\'s EIN'
                     }
                 },
 
@@ -74,11 +73,10 @@ var OrgForm = function () {
                 submitHandler: function(form)
                 {
                     
-                    $(document).on('submit','#orgForm', function(e){
-                        e.preventDefault();
-                        var data = $(this).serializeArray();
+                        var orgForm = $(document).find('#orgForm');
+                        var data = $(orgForm).serializeArray();
                         var request = $.ajax({
-                            url: 'https://app.greathearts.community/api/v1/organizations/org_recommendation',
+                            url: 'http://localhost:3000/api/v1/organizations/org_recommendation',
                             method: 'post',
                             data: data,
                             crossDomain: true
@@ -88,8 +86,6 @@ var OrgForm = function () {
                             $('#orgForm').remove();
                             $('.success-msg').addClass('flex-center-content').show();
                         });
-                    });
-                    Stamplay.init("greathearts");
 
                 },
 
